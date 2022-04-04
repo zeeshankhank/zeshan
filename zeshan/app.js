1e385a3446ee6ef7a3184e710814fc428f77a158
Zeshan khan <98458839+zeeshankhank@users.noreply.github.com>
2022-04-04 17:58:30 +0500

    Add files via upload

---
+++ zeshan/app.js
@@ -0,0 +1,325 @@
+// /(chatper no 1)
+
+// (question no 1)
+
+// alert("HELLO USER");
+
+// question no 2
+
+
+// alert("Error! please inter a valid passwors ");
+
+// /question no 3
+
+// alert("Wellcome to js land \n happy coding!")
+
+// question no 4
+
+// alert("wellcome to js land...")
+// alert("happy coding..\n prevent this page from a creating additonnal dialogs.")
+
+// question no 5
+
+// alert("hello.. i can run js thought my web browser console")
+
+// question no 6
+// answer
+// alert("new/existing HTML & CSS project")
+
+// question n0 7
+
+//  Practice placement of <script></script> element in 
+// following sections of your project in exercise 6:
+// answer 
+// body (inside your page’s HTML)
+
+// capter no 2
+
+// question no 1
+
+// var username;
+
+// var myName = "muhammd ubaid";
+
+// var name = "muhammdubaid";
+
+// question no 2
+
+// var firstName = "ubaid";
+
+// console.log("hello" , firstName);
+
+// question no 3
+// var name = "message"
+// var lastName = "Hello world"
+// alert(name)
+// alert(lastName)
+
+
+
+// question no 4
+
+// var myName = "Jhone doe"
+// var age = "18" 
+// var lastName = " certifeid mobile application development" 
+// alert(myName)
+// alert(age)
+// alert(lastName)
+ 
+// question no 5
+
+// var myName = ("PIZZA \n PIZZ \nPLZ \n PL \n P")
+// alert(myName)
+
+// question no 6
+
+// var email =("my email dress is example@example.com ")
+// alert(email)
+ 
+// question no 7
+
+// var word =("i am trying a learn from the book A book master way to learn javasricpt")
+// alert(word)
+
+// question no 8
+
+// var word;
+// document.write( "<h1>yAh! i can Write contant though javascript</h1>");
+
+// question no 9
+
+// var word = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”"
+// alert(word)
+
+// chapter no 3
+
+// question no 1
+
+// var age = " i am 15 year old"
+// alert(age)
+
+// question no 2
+
+// var num ="you have visited this site 14 times"
+// alert(num)
+
+// qustion no 3
+// var num;
+// document.write("<h2>my birth year id 1990</h2> \n <h2>Data type of my  decleared varible is number</h2>")
+
+// question no 4
+
+// var name;
+// document.write("<b>Jhone Doe</b> order <b>5 T-shir(s) on XYZ Closing store </b> ")
+
+// chapter no 4
+
+// question no 1
+// var firstName="muhammd",
+// lastName="ubaid",
+// place="karachi";
+// alert(firstName)
+// alert(lastName)
+// alert(place)
+// console.log(firstName)
+// console.log(lastName)
+// console.log(place)
+
+// question no 2
+
+//  var name = ("ubaid,danish,naomi,shani,ahsan") //   legal
+//  var num = (" 1,2,3,4,5") //illegal 
+//  alert(name)
+//  alert(num)
+
+// question no 3
+// var myName;
+// document.write("<h1>Rules for naming JS variables</h1> <br>")
+// document.write("Variable names can only contain ,number $ For example $my_1stVariable <br>")
+// document.write(" Variables must begin with a letter $ or _ For example $name, _name or name <br>")
+// document.write("Variable names are case sensitive <br>")
+// document.write(" Variable names should not be JS keyboard")
+
+// chapte no 5 
+
+// question  no 1
+// var num1 = 5;
+// var num2 = 3;
+// var sum = num1 + num2;
+// document.write('The sum of ' + num1 + ' and ' + num2 + ' is: ' + sum);
+
+// question no 2
+// var num1 = +prompt("Enter First Number");
+// var num2 = +prompt("Enter Second Number");
+// var num1 = (20)
+// var num2 = (30)
+// var add = num1 + num2;
+// console.log(add);
+// var sub = num1 - num2;
+// console.log(sub);
+// var product = num1 * num2;
+// console.log(product);
+// var divide = num1 / num2
+// console.log(divide);
+// var mod = num1 % num2
+// console.log(mod);
+
+// question n0 3
+// var number;
+// document.writeln("Value after variable declaration is: "+number)
+// number=5;
+// document.writeln("<br/>Initial value is: "+number)
+// number++;
+// document.writeln("<br/>Value after increment is: "+number)
+// number+=7;
+// document.writeln("<br/>Value after addition is: "+number)
+// number--;
+// document.writeln("<br/>Value after decrement is: "+number)
+// number%=3
+// document.writeln("<br/>The remainder is: "+number)
+
+// question n0 4
+
+// var ticket_price=600;
+// document.write("Total cost to buy 5 tickets to a movie is "+ticket_price*5+" PKR")
+
+// question n0 5
+
+// for (let i = 1; i <=10; i++) {
+//     document.write("<br> 4x"+i+"="+4*i)
+// }
+//var table_no=4;
+// document.write(table_no+"x1="+table_no*1)
+// document.write("<br>"+table_no+"x2="+table_no*2)
+// document.write("<br>"+table_no+"x3="+table_no*3)
+// document.write("<br>"+table_no+"x4="+table_no*4)
+// document.write("<br>"+table_no+"x5="+table_no*5)
+// document.write("<br>"+table_no+"x6="+table_no*6)
+// document.write("<br>"+table_no+"x7="+table_no*7)
+// document.write("<br>"+table_no+"x8="+table_no*8)
+// document.write("<br>"+table_no+"x9="+table_no*9)
+// document.write("<br>"+table_no+"x10="+table_no*10)
+
+
+
+// question n0 6
+// var celsius=25;
+// var farenheit=(celsius*1.8)+32
+// document.write(celsius+"oC is "+farenheit+"oF <br>")
+// var farenheit2=70;
+// var convert_faren=(farenheit2-32)*5/9
+// document.write(farenheit2+"oF is "+convert_faren+"oC")
+
+// question n0 7
+
+// var price_item1=650;
+// var price_item2=100;
+// var quantity_item1=3;
+// var quantity_item2=7;
+// var shipping_charges=100;
+// var total=(price_item1*quantity_item1)+(price_item2*quantity_item2)+shipping_charges;
+// document.write("Price of item 1 is "+price_item1)
+// document.write("<br>Quantity of item 1 is "+quantity_item1)
+// document.write("<br>Price of item 2 is "+price_item2)
+// document.write("<br>Quantity of item 2 is "+quantity_item2)
+// document.write("<br>Shipping Charges "+shipping_charges)
+// document.write("<br><br>Total Cost of your order is "+total);
+
+
+// question n0 8
+
+// var total_marks=980;
+// var obtained_marks=804;
+// document.write("Total Marks: "+total_marks)
+// document.write("<br>Obtained Marks: "+obtained_marks)
+// document.write("<br>Percentage: "+obtained_marks/total_marks*100)
+
+// question n0 9
+
+// var us_dollar=10;
+// var saudi_riyals=25;
+// var single_dollar=104.80;
+// var single_riyal=28;
+// var total=(us_dollar*single_dollar)+(saudi_riyals*single_riyal);
+// document.write("Total Currency in PKR "+total)
+
+// question n0 10
+
+// var number=15;
+// var operations=number+5*10/2;
+// document.write(operations)
+
+//question no 11
+
+// var current_year=2022;
+// var birth_year=1999;
+// var age=current_year-birth_year;
+// document.write("Your age is "+age)
+
+//question  no 12
+
+// var radius=20;
+// var circumference=2*3.142*radius;
+// var area=3.142*(radius*radius)
+// document.write("Radius of circle is: "+radius)
+// document.write("<br>The circumference is: "+circumference)
+// document.write("<br>The area is: "+area)
+
+//question no 13
+
+// var fav_snack="chocolate";
+// var current_age=15;
+// var max_age=65;
+// var est_amount_day=3;
+// var est_snacks=(max_age-current_age)*est_amount_day;
+// document.write("Favourite Snack: "+fav_snack)
+// document.write("<br>Current Age: "+current_age)
+// document.write("<br>Estimated Maximum Age: "+max_age)
+// document.write("<br>Amount of snack per day: "+est_amount_day)
+// document.write("<br>Your will need "+est_snacks+" "+fav_snack+" to last you until the ripe old age of "+max_age)
+
+// chapter no 6-9
+
+// question no 1
+
+// document.write("<h2>result</h2> <br> the value of a is 10 <hr>")
+// var num1 = 10
+// num1++
+// document.write("the value  of " +num1)
+// ++num1
+// document.write("<br>the value  of " +num1)
+// --num1
+// document.write("<br>the value  of " +num1)
+// num1--
+// document.write("<br>the value  of " +num1)
+
+// question no 2
+
+// var a = 2, b = 1;
+// --a
+// document.write(" <br>a is " + --a )
+// --a - --b;
+// document.write("<br>a is " + a , b)
+// --a - --b + ++b
+// document.write("<br> result a is " + a , b)
+
+// question no 6 
+
+
+// var sub1 = +prompt("enter english marks");
+// var sub2 = +prompt("enter urdu marks");
+// var sub3 = +prompt("enter math marks");
+// var sub4 = +prompt("enter html marks");
+// var sub5 = +prompt("enter javascript marks");
+
+// var obtMarks = sub1 + sub2 + sub3 + sub4 + sub5;
+// var totalMark = 500;
+// var percentage = (obtMarks / totalMark) * 100;
+// var percentage = 79;
+
+// percentage >= 70 && percentage <=79 (A)
+// percentage >= 60 && percentage <=69 (b)
+// percentage >= 50 && percentage <=59 (c)
+// percentage >= 0 && percentage <=49 (F)
+
+// Document.write  ("percentage");
